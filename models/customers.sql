9df93fd6496da6606ffe75a81abcfd0da3918344
Carlin Leungcd <carlin.leung@imaginecurve.com>
2020-11-18 13:54:19 +0000

    set up payments staging table and added lifetime value field in customers model

--- models/customers.sql
+++ models/customers.sql
@@ -4,44 +4,28 @@
         )
 }}
 
-with customers as (
-    SELECT * FROM {{ ref('stg_customers')}}
-),
+WITH customer_orders AS (
+    SELECT customer_id,
+           MAX(order_date) AS first_order_date,
+           MAX(order_date) AS most_recent_order_date,
+           COUNT(order_id) AS number_of_orders,
+           SUM(payment_amount) AS total_order_value
 
-orders as (
-    SELECT * FROM {{ ref('stg_orders')}}
-),
+    FROM {{ ref('orders')}} orders
+        WHERE payment_status = 'success'
 
-customer_orders as (
-
-    select
-        customer_id,
-
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
-
-    from orders
-
-    group by 1
-
-),
-
-
-final as (
-
-    select
-        customers.customer_id,
-        customers.first_name,
-        customers.last_name,
-        customer_orders.first_order_date,
-        customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
-
-    from customers
-
-    left join customer_orders using (customer_id)
-
+    GROUP BY 1
 )
 
-select * from final
+SELECT customers.customer_id,
+       customers.first_name,
+       customers.last_name,
+       customer_orders.first_order_date,
+       customer_orders.most_recent_order_date,
+       COALESCE(customer_orders.number_of_orders, 0) AS number_of_orders,
+       SUM(customer_orders.total_order_value::float) AS lifetime_value
+
+FROM {{ ref('stg_customers')}} customers
+    LEFT JOIN customer_orders USING (customer_id)
+
+GROUP BY 1, 2, 3, 4, 5, 6
--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,6 +1,5 @@
-select
-    id as customer_id,
-    first_name,
-    last_name
+SELECT id AS customer_id,
+       first_name,
+       last_name
 
-from raw.jaffle_shop.customers
+FROM raw.jaffle_shop.customers
